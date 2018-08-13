8928570b6bdfaa5337dd900540e7e3a2ecd09475
fireflower0 <fireflower0@hotmail.co.jp>
2018-08-13 22:36:35 +0900

    Preparation for drawing processing by callback function.

--- cvxfield.c
+++ cvxfield.c
@@ -1,21 +1,27 @@
 #include <stdlib.h>
 #include "cvxfield.h"
 
+static gboolean cvx_field_expose_event(GtkWidget* widget, GdkEventExpose* event, gpointer user_data){
+    g_print("exposed!\n");
+
+    return FALSE;
+}
+
 CvxField* cvx_field_new(GtkWindow* window, gchar* title, guint width, guint height){
-    CvxField* retval;
+    CvxField*  retvar;
     GtkWidget* canvas;
 
-    retval = (CvxField*)malloc(sizeof(CvxField));
-
-    if(retval == NULL) return NULL;
+    retvar = (CvxField*)malloc(sizeof(CvxField));
+    if(retvar == NULL) return NULL;
 
     gtk_window_set_title(window, title);
     gtk_widget_set_size_request(GTK_WIDGET(window), width, height);
-    retval->window = window;
+    retvar->window = window;
 
     canvas = gtk_drawing_area_new();
     gtk_container_add(GTK_CONTAINER(window), canvas);
-    retval->canvas = canvas;
+    g_signal_connect(G_OBJECT(canvas), "expose-event", G_CALLBACK(cvx_field_expose_event), retvar);
+    retvar->canvas = canvas;
 
-    return retval;
+    return retvar;
 }
